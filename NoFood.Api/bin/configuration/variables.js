d20b10f25e5bc64ff95e4d9c6435996318917fe7
salvego <56658747+salvego@users.noreply.github.com>
2022-01-14 12:47:06 -0300

    Update variables.js

--- NoFood.Api/bin/configuration/variables.js
+++ NoFood.Api/bin/configuration/variables.js
@@ -3,10 +3,10 @@ const variables = {
         port: process.env.port || 3000
     },
     Database: {
-        connection: process.env.connection || 'mongodb://admin:#abc123@ds257981.mlab.com:57981/nofood'
+        connection: process.env.connection || 'MINHA_STRING_API'
     },
     Security: {
-        secretyKey: 'd41d8cd98f00b204e9800998ecf8427e|7aef61337bcee2fe773aa78b40afacbc'
+        secretyKey: 'MINHA_SECRETY_KEY'
     }
 }
 module.exports = variables;
